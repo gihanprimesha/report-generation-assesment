1f3286d6eb8d61d662475d4e447ef51c7bec014a
Gihan Primesha <gihan@ideahub.lk>
2021-05-10 09:35:59 +0530

    Change use name password

--- Configs/local.php
+++ Configs/local.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-    'user_name' => 'root',
-    'password' => 'Think100%',
+    'user_name' => '',
+    'password' => '',
     'server_name' => '127.0.0.1',
     'db_name' => 'otrium_challenge2'
 );
